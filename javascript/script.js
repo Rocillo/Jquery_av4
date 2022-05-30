a933cf84f509bd217936c19f9d18c2d0676838a6
Rocillo <guilherme.rocillo@sp.senai.br>
2022-05-30 19:18:55 -0300

    300522

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $("div p").eq(0).css({"color":"red"});
+    $("div p").eq(1).css({"color":"green","font-size":"40px","text-align":"center"});
+    $("div p").eq(2).css({"color":"blue","font-size":"30px"});
+});
+    
